e7f9e214110c3e6d56a4e9adf3cec2193c6a2e35
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-07-06 18:34:55 -0300

    tree blind 75 exercies

---
+++ blind-75/tree/kSmallestElementBST.py
@@ -0,0 +1,42 @@
+"""Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # create a helper function
+        def inorder(root,k,res):
+            # base case
+            if root is None:
+                return
+            #general case
+            inorder(root.left,k,res)
+            res.append(root.val)
+            inorder(root.right,k,res)
+            return res
+        # call the helper function
+        return inorder(root,k,[])[k-1]
