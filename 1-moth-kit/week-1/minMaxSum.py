770558be7cf30d88d4de9475d5f9e679bdcf47a9
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-07-11 20:14:28 -0300

    start a projects

---
+++ 1-moth-kit/week-1/minMaxSum.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    maxSum = sum(arr[1:])
+    minSum = sum(arr[0:-1])
+    print(minSum,maxSum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
