bea08a4ae3a1ea5a166e62e9f3c03c3c6f755f8e
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-07-18 12:46:22 -0300

    new exercices

---
+++ 1-moth-kit/salesByMatch.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    numberOfPairs = 0
+    socks = {}
+    for i in ar:
+        if i in socks:
+            numberOfPairs += 1
+            socks.pop(i)
+        else:
+            socks[i] = 1
+    return numberOfPairs
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
