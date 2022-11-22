e73562073126346081cbfb9e93454a8773ff2a95
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-11-22 08:08:52 -0300

    new exefcieses

---
+++ 1-month-kit/week-2/gridChallenge.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    sorted_grid = [sorted(row) for row in grid]
+    for column in range(len(grid[0])):
+        last = sorted_grid[0][column]
+        for row in range(1, len(grid)): 
+            if sorted_grid[row][column] < last:
+                return 'NO'
+    return 'YES'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
