3f1499a32beb6d3bfec0dbba8de683629cd2ddd8
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-09-27 21:11:00 -0300

    add fgv alg

---
+++ courses/alg-py-interviews/graph-algorithims/dfg-traversal.py
@@ -0,0 +1,113 @@
+
+def dfs(my_graph, source):
+    """
+    Function to print a DFS of graph
+    :param graph: The graph
+    :param source: starting vertex
+    :return: returns the traversal in a string
+    """
+    
+    # Mark all the vertices as not visited
+    visited = [False] * (len(my_graph.graph))
+
+    # Create a stack for DFS
+    stack = []
+
+    # Result string
+    result = ""
+
+    # Push the source
+    stack.append(source)
+
+    while stack:
+
+        # Pop a vertex from stack
+        source = stack.pop()
+        
+        if not visited[source]:
+            result += str(source)
+            visited[source] = True
+
+        # Get all adjacent vertices of the popped vertex source.
+        # If a adjacent has not been visited, then push it
+        while my_graph.graph[source] is not None:
+            data = my_graph.graph[source].vertex
+            if not visited[data]:
+                stack.append(data)
+            my_graph.graph[source] = my_graph.graph[source].next
+
+    return result
+
+
+# Main to test the above program
+if __name__ == "__main__":
+    
+    V = 5
+    g = Graph(V)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 3)
+    g.add_edge(1, 4)
+
+    print(dfs(g, 0))
+
+
+
+class AdjNode:
+    """
+    A class to represent the adjacency list of the node
+    """
+
+    def __init__(self, data):
+        """
+        Constructor
+        :param data : vertex
+        """
+        self.vertex = data
+        self.next = None
+
+
+class Graph:
+    """
+    Graph Class ADT
+    """
+
+    def __init__(self, vertices):
+        """
+        Constructor
+        :param vertices : Total vertices in a graph
+        """
+        self.V = vertices
+        self.graph = [None] * self.V
+
+    def add_edge(self, source, destination):
+        """
+        add edge
+        :param source: Source Vertex
+        :param destination: Destination Vertex
+        """
+
+        # Adding the node to the source node
+        node = AdjNode(destination)
+        node.next = self.graph[source]
+        self.graph[source] = node
+
+        # Adding the source node to the destination if undirected graph
+        
+        # Intentionally commented the lines
+        #node = AdjNode(source)
+        #node.next = self.graph[destination]
+        #self.graph[destination] = node
+
+    def print_graph(self):
+        """
+        A function to print a graph
+        """
+        for i in range(self.V):
+            print("Adjacency list of vertex {}\n head".format(i), end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+            
