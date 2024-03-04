648f0bbb203e28240ac60555209c974aa975da68
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-03-04 18:22:21 -0300

    Add maximumSubarray solution using divide and conquer approach

---
+++ grind-75/week-2/maximumSubarray.py
@@ -0,0 +1,89 @@
+"""
+
+Maximum Subarray
+Given an integer array nums, find the  subarray with the largest sum, and return its sum.
+
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+"""
+import math
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        max_sum = float('-inf')  
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i, n):
+                current_sum = sum(nums[i:j+1])
+                max_sum = max(max_sum, current_sum)
+
+        return max_sum
+            
+    def maxSubArray2(self, nums: List[int]) -> int:
+        current_subarray = max_subarray = nums[0]
+
+        for num in nums[1:]:
+            current_subarray = max(num, current_subarray + num)
+            max_subarray = max(max_subarray, current_subarray)
+        
+        return max_subarray
+
+    def maxSubArray3(self, nums: List[int]) -> int:
+        def findBestSubarray(nums, left, right):
+            # Base case - empty array.
+            if left > right:
+                return -math.inf
+
+            mid = (left + right) // 2
+            curr = best_left_sum = best_right_sum = 0
+
+            # Iterate from the middle to the beginning.
+            for i in range(mid - 1, left - 1, -1):
+                curr += nums[i]
+                best_left_sum = max(best_left_sum, curr)
+
+            # Reset curr and iterate from the middle to the end.
+            curr = 0
+            for i in range(mid + 1, right + 1):
+                curr += nums[i]
+                best_right_sum = max(best_right_sum, curr)
+
+            # The best_combined_sum uses the middle element and
+            # the best possible sum from each half.
+            best_combined_sum = nums[mid] + best_left_sum + best_right_sum
+
+            # Find the best subarray possible from both halves.
+            left_half = findBestSubarray(nums, left, mid - 1)
+            right_half = findBestSubarray(nums, mid + 1, right)
+
+            # The largest of the 3 is the answer for any given input array.
+            return max(best_combined_sum, left_half, right_half)
+        
+        # Our helper function is designed to solve this problem for
+        # any array - so just call it using the entire input!
+        return findBestSubarray(nums, 0, len(nums) - 1)
