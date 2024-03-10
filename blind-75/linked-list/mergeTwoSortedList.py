5aae7ed5c595b897c0a462d4d1b52d3519de3d99
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-03-10 19:00:01 -0300

    Add solution for merging two sorted linked lists

---
+++ blind-75/linked-list/mergeTwoSortedList.py
@@ -0,0 +1,73 @@
+"""
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in no
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = new_list = ListNode(0)
+        
+        
+        while (list1 and list2):
+            if list1.val < list2.val:
+                new_list.next = list1
+                list1 = list1.next
+            else:
+                new_list.next = list2
+                list2= list2.next
+            new_list = new_list.next
+        new_list.next = list1 or list2
+        return head.next
+    
+    
+    def mergeTwoListsRecursion(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if list1 is None:
+            return list2
+        
+        if list2 is None:
+            return list1
+        
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoListsRecursion(list1.next, list2)
+            return list1
+        
+        else:
+            list2.next = self.mergeTwoListsRecursion(list1, list2.next)
+            return list2
+        
+        
