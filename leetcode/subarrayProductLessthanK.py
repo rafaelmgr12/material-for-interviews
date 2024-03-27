9002d701dd68ed08ad38cdd1cef10875439d355a
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-03-27 10:21:28 -0300

    Add solution for Subarray Product Less Than K problem

---
+++ leetcode/subarrayProductLessthanK.py
@@ -0,0 +1,65 @@
+"""
+713. Subarray Product Less Than K
+
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+ 
+
+Example 1:
+
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Example 2:
+
+Input: nums = [1,2,3], k = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+1 <= nums[i] <= 1000
+0 <= k <= 106
+"""
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <=1:
+            return 0
+        
+        count = 0
+        product = 1
+        
+        left = 0
+        for right in range(len(nums)):
+            product *= nums[right]
+            while product >= k:
+                product /= nums[left]
+                left += 1
+            count += right - left + 1
+        return count
+        
+    
+    
+    def numSubarrayProductLessThanKBruteForce(self, nums: List[int], k: int) -> int:
+        
+        def dfs(start, end, product, k):
+            if product >= k:
+                return 0
+            if end == len(nums):
+                return 0
+            if start == end:
+                product = nums[start]
+            else:
+                product *= nums[end]
+            if product >= k:
+                return 0
+            return 1 + dfs(start, end + 1, product, k)
+        
+        count = 0
+        for i in range(len(nums)):
+            count += dfs(i, i, 0, k)
+        return count
