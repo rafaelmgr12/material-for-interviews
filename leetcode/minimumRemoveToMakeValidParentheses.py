1593163aeb168dc79abe2303c58b4ae3929060ad
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-06 13:24:26 -0300

    Add solution for Minimum Remove to Make Valid Parentheses problem

---
+++ leetcode/minimumRemoveToMakeValidParentheses.py
@@ -0,0 +1,57 @@
+"""
+1249. Minimum Remove to Make Valid Parentheses
+Medium
+Topics
+Companies
+Hint
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either'(' , ')', or lowercase English letter.
+"""
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+        stack = []
+        s = list(s)
+       
+        for i in range(len(s)):
+            if s[i] == "(":
+               stack.append(i)
+            elif s[i] == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ""
+        for index in stack:
+            s[index] = ''
+        return ''.join(s)
+                
