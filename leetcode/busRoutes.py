2ba36934b7b6dbcdd6376bbacc040e7dec192bd9
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-04-11 16:31:54 -0300

    Add solution for Bus Routes problem

---
+++ leetcode/busRoutes.py
@@ -0,0 +1,75 @@
+"""
+815. Bus Routes
+
+You are given an array routes representing bus routes where routes[i] is a bus route that the ith bus repeats forever.
+
+For example, if routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
+You will start at the bus stop source (You are not on any bus initially), and you want to go to the bus stop target. You can travel between bus stops by buses only.
+
+Return the least number of buses you must take to travel from source to target. Return -1 if it is not possible.
+
+ 
+
+Example 1:
+
+Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+Output: 2
+Explanation: The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
+Example 2:
+
+Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+Output: -1
+ 
+
+ 
+
+Constraints:
+
+1 <= routes.length <= 500.
+1 <= routes[i].length <= 105
+All the values of routes[i] are unique.
+sum(routes[i].length) <= 105
+0 <= routes[i][j] < 106
+0 <= source, target < 106
+
+"""
+
+
+from collections import defaultdict, deque
+
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        graph = defaultdict(set)
+        for i, route in enumerate(routes):
+            for stop in route:
+                graph[stop].add(i)
+
+        
+        
+        
+        queue = deque([source])
+        seen_stop = set()
+        seen_route = set()
+        ans = 0
+
+        seen_stop.add(source)
+
+        while queue:
+            for _ in range(len(queue)):
+                stop = queue.popleft()
+
+                if stop == target:
+                    return ans
+                for routeId in graph[stop]:
+                    if routeId not in seen_route:
+                        for new_stop in routes[routeId]:
+                            if new_stop not in seen_stop:
+                                queue.append(new_stop)
+                                seen_stop.add(new_stop)
+                        seen_route.add(routeId)
+            ans +=1
+        return -1
+            
+
+
